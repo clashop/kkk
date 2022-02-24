aedab0f7c54cba783852717f6e5757c2ebe5812e
clashop <76790384+clashop@users.noreply.github.com>
2022-02-25 00:04:36 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  Token: "NTI5MjAwMjc4NjEyNzM4MDQ5.YhDlDA.LJh7C4_GntKMukQDrCC2CAvstDc",  // write your selfbot discord account token
+  Prefix: "!", // customizer
+  BotOwner: "852892410240696322", // write the user id of your own discord account.
+  OwoBotID: "408785106942164992", // Default Do Not Change!
+  UserID: ["941754488723742742","941755419238473738","941756138662264852","941377917164847125"], // Sometimes he will tag other users so that the account is not banned from the owo bot, write different discord account ids here.
+  Writes: ["Test Message :)","Spam Message","Hello World!","Hi Guys :)","You Are Noob!"], // In order to prevent the account from being banned from the owo bot, it sends random messages from time to time, in this section there are random messages it will send.
+  GuildID: "944304293387849788" // Type id of the discord server where the selfbot will run.
+};
